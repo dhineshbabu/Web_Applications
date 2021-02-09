fa7912f733774a9b0f05fc469f071f58a87d2459
dhinesh <dhineshbabu89@gmail.com>
2021-02-09 18:22:47 +0530

    added some basic js code

---
+++ Javascript/Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,83 @@
+
+/*
+let js = "amazing";
+console.log(40+8+23-10);
+console.log("Dhinesh");
+console.log(js);
+*/
+
+// Datatypes
+/*
+----- Primitive Types -----
+Number: Floating point numbers always
+String: sequence of characters
+Boolean
+Undefined
+null
+Symbol (introduced in ES2015)
+BigInt (ES2020)
+*/
+/*
+console.log(typeof true); // boolean
+let someBoolean = true;
+console.log(someBoolean);
+console.log(typeof "Dhinesh");
+someBoolean = false;
+console.log(someBoolean);
+
+//undefined
+let thisIsUndefined;
+console.log(typeof thisIsUndefined);
+
+//null
+console.log( typeof null ); //object - this is a bug in js
+
+//block scope
+let age = 30;
+age = 31; 
+
+const testConstant = 1991;
+
+//function scoped
+var testVar = 'dhinesh';
+var testVar = 'kanu';
+console.log(testVar);
+
+const firstName = "Dhinesh";
+const lastName = "Babu";
+const fullName = `Hello ${firstName} ${lastName}`;
+console.log(fullName);
+
+console.log(`Multi line 
+string 
+literals`);
+*/
+
+// Type Conversion
+/*
+const inputYear = '1991';
+console.log(Number(inputYear) + 18);
+console.log(String(23));
+*/
+
+// Type Coercion
+/*
+console.log("I am " + 23 + " years old.");
+// 5 falsy values '', undefined, 0, null, NaN
+
+const fav = prompt("What is your favorite number ?");
+console.log(fav + );
+*/
+/*
+const day = 'monday';
+
+switch(day) {
+    case 'monday':
+        console.log("This is monday");
+        break;
+    case 'tuesday':
+
+}
+*/
+
+
---
+++ Javascript/Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,49 @@
+/* 
+'use strict';
+let hasdriversLicense = false;
+const passTest = true;
+
+if(passTest) hasdriversLicense = true;
+if(hasdriversLicense) console.log("I can drive");
+
+const interface = 124;
+*/
+
+//Functions
+
+/*
+
+function logger() {
+    console.log("This is from a function");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(`we have ${apples} apples and ${oranges}`);
+}
+
+fruitProcessor(4,5);
+
+console.log(23);
+
+
+let sum = (a,b) => console.log(a+b);
+sum(10,12);
+
+const sum2 = function(a,b) {
+    return a+b;
+}
+
+console.log(sum2(12,2));
+
+*/
+
+//Arrays
+
+const friends = ["Dhinesh", "Kanu", "Bru"];
+
+friends.forEach(element => {
+    console.log(element);
+});
+ 
